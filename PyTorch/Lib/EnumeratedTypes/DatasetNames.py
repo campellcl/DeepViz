d0473700cc4917a8fbb1eecc89b643e33913dd3b
ccampell <ccampell.asu@gmail.com>
2020-08-15 22:08:59 -0400

    Added missing imports.

---
+++ PyTorch/Lib/EnumeratedTypes/DatasetNames.py
@@ -0,0 +1,6 @@
+from enum import Enum
+
+
+class DatasetNames(Enum):
+    CIFAR_TEN = 'CIFAR_10'
+
---
+++ PyTorch/Lib/EnumeratedTypes/DatasetTypes.py
@@ -0,0 +1,9 @@
+from enum import Enum
+
+
+class DatasetTypes(Enum):
+
+    TRAIN = 'train'
+    VAL = 'val'
+    TEST = 'test'
+
